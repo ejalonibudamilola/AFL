d58a729f8bc7438beaf9d87ff0241b04201619f1
Damilola Ejalonibu <ejalonibudamilola96@gmail.com>
2023-01-26 18:58:33 +0100

    code update

---
+++ index.php
@@ -0,0 +1,451 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>AFL</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <link href="assets/img/AFL.png" rel="icon">
+  <link href="assets/img/AFL.png" rel="apple-touch-icon">
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <!-- ======= Header ======= -->
+  <header id="header" class="header d-flex align-items-center fixed-top">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+
+      <a href="index.html" class="logo d-flex align-items-center">
+        <img src="assets/img/logo.png" alt="">
+      </a>
+
+      <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+      <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href="#hero" class="active">Home</a></li>
+          <li><a href="#about">About Me</a></li>
+          <li><a href="#book">Media Library</a></li>
+          <li><a href="#counsel">Book a Session</a></li>
+          <li><a href="#contact">Invite Me</a></li>
+          <li><a href="#give">Give</a></li>          
+        </ul>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Slide Section ======= -->
+  <section id="hero" class="hero d-flex align-items-center">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-5">
+          <h2 data-aos="fade-up">Apostle Femi Lazarus</h2>
+          <blockquote data-aos="fade-up" data-aos-delay="100">
+            <p>
+              A man saddled with the mandate to raise  God's end time armies
+              in all sphere of influence. 
+            </p>
+          </blockquote>        
+
+        </div>
+        
+      </div>
+    </div>
+  </section>
+  <!-- End Slide Section -->
+  <main id="main">
+
+    <!-- ======= About Us Section ======= -->
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>About Me</h2>
+          <p>
+            Apostle Femi Lazarus is the lead pastor of Sphere of Light Church. He is an Apostle
+            with a prophetic cutting edge. He has the mandate to raise God's end time armies.
+            Apostle Femi Lazarus is a public health professional by training. He is the principal
+            of the international School of Ministry, a platform through which he has trained ministers 
+            from more than thirty nations. He is a lover of God. He is married to Pastor Miracle
+            Femi-Lazarus and they are blessed with a daughter Abigail Lazarus.
+
+          </p>
+
+        </div>                 
+      </div>
+
+      </div>
+    </section>
+    <!-- End About Us Section -->
+
+    <!-- ======= Book Section ======= -->
+    <section id="book" class="book">
+      <div class="container" data-aos="fade-up">
+        <div class="section-title">
+          <h2>My Books</h2>
+        </div>
+        <div class="row">
+          <div class="col-lg-6 d-flex align-items-center" data-aos="fade-right" data-aos-delay="100">
+            <img src="assets/img/pb.png" class="img-fluid book-img" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left" data-aos-delay="100">
+            <h3>The final move of God</h3>
+            <p class="book-text">
+              I am very certain of my role in the last day church. My role is to help preserve
+              the harvest. Here is a book God has inspired me to write, here is a guide
+              on how to preserve the move of God in Africa, and how to maximize what God is doing
+              with Africa currently. A season is upon us, and I believe this book will be relevant
+              in the church, both now and in years to come. Get the book with the links below.
+            </p>
+            <div class="text-center">
+              <a target="_blank" href="https://selar.co/femilazarus"><button style="width:40%" class="moremsg"><img style="width:20%" src="assets/img/selar.png"/></button></a>
+              <a target="_blank" href="https://store.okadabooks.com/book/about/the_final_move_of_god/51544"><button style="width:40%" class="moremsg"><img style="width:25%" src="assets/img/okada.png"/></button></a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <!-- End Book Section -->
+
+    <!-- ======= Message Section ======= -->
+    <section id="message" class="message section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>My Messages</h2>
+          <p>
+            With Apostle Femi Lazarus, every minute counts as God has placed His Wisdom,
+            and that which is needed to navigate the body of Christ in this season on him.
+            Listen attentively to these messages and you will not lack light and direction.
+          </p>
+        </div>
+        <div class="row message-div">
+          <div class="col-md-2"></div>
+          <div class="col-md-8">
+            <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic">
+                <img src="assets/img/vector.png" class="img-fluid" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Encountering God As The El-Shaddai</h4>
+                <span>Apostle Femi Lazarus</span>
+                <p><audio src="https://t.me/Femilazarusmessages/861" controls></audio></p>
+                
+              </div>
+            </div>
+          </div>
+          <div class="col-md-2"></div>
+        </div> 
+
+        <div class="row message-div">
+          <div class="col-md-2"></div>
+          <div class="col-md-8">
+            <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic">
+                <img src="assets/img/vector.png" class="img-fluid" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Three Accurate Response To Give To The Love Of God</h4>
+                <span>Apostle Femi Lazarus</span>
+                <p><audio src="https://t.me/Femilazarusmessages/862" controls></audio></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-2"></div>
+        </div> 
+
+        <div class="row">
+          <div class="col-md-2"></div>
+          <div class="col-md-8">
+            <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic">
+                <img src="assets/img/vector.png" class="img-fluid" alt="">
+              </div>
+              <div class="member-info">
+                <h4>They That Know Their God</h4>
+                <span>Apostle Femi Lazarus</span>
+                <p><audio src="https://t.me/Femilazarusmessages/857" controls></audio></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-2"></div>
+        </div>      
+        <div class="row">
+          <div class="col-md-2"></div>
+          <div class="col-md-8 text-center">
+            <a target="_blank" href="https://open.spotify.com/show/6jmIgwrRXKg0kpRlVJNbwh?si=UtjHcS_2SNWLexKom5LJKg&utm_source=copy-link"><button class="moremsg"><i class="bx bxl-spotify"></i>GET MORE ON SPOTIFY</button></a>
+            <a target="_blank" href="https://t.me/Femilazarusmessages"><button class="moremsg"><i class="bx bxl-telegram"></i> GET MORE ON TELEGRAM</button></a>
+          </div>
+          <div class="col-md-2"></div>
+        </div>
+      </div>
+    </section>
+    <!-- End Message Section -->
+
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Invitation to Meetings</h2>
+          <p>
+            Please note that if your invitation is accepted, a round trip flight has
+            to be booked for two people.
+          </p>
+        </div>
+
+        <div class="row">          
+          <div class="col-lg-12 mt-5 mt-lg-0 d-flex align-items-stretch">
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+              <div class="row">
+                <div class="form-group col-md-6">
+                  <label for="name">Ministry Name</label>
+                  <input type="text" name="mname" class="form-control" id="mname" required>
+                  <span id="cmname" class="error" font-weight-bold></span>
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="name">Location</label>
+                  <input type="text" class="form-control" name="location" id="location" required>
+                  <span id="clocation" class="error" font-weight-bold></span>
+                </div>
+              </div>
+              <div class="row">
+                <div class="form-group col-md-6">
+                  <label for="name">Meeting Date</label>
+                  <input type="date" name="mdate" class="form-control" id="mdate" required>
+                  <span id="cdate" class="error" font-weight-bold></span>
+                </div>               
+                <div class="form-group col-md-6">
+                  <label for="name">Phone Number</label>
+                  <input type="text" name="number" class="form-control" id="number" required>
+                  <span id="cnumber" class="error" font-weight-bold></span>
+                </div>
+              </div>              
+              <div class="form-group">
+                <label for="name">Email</label>
+                <input type="email" name="email" class="form-control" id="email" required>
+                <span id="cemail" class="error" font-weight-bold></span>
+              </div>            
+             
+              <div class="text-center"><button id="invite" type="button">Send Invite</button></div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    
+
+    <div class="footer-newsletter" id="counsel">
+      <div class="container" data-aos="fade-up">
+        <div class="row justify-content-center">
+          <div class="col-lg-12" data-aos="zoom-in" data-aos-delay="100">
+            <h4>Book a counselling session</h4>
+            <form action="" method="post">
+              <div class="row">
+                <div class="col-md-5">
+                  <div class="form-group col-md-12 session">
+                    <input type="text" name="fname"  placeholder="Name" id="fname">
+                  </div>
+                </div>
+                <div class="col-md-5">
+                  <div class="form-group col-md-12 session">
+                    <input type="text" name="phone" placeholder="Phone Number" id="phone">
+                  </div>
+                </div>
+                <div class="col-md-2">
+                  <div class="form-group col-md-12">
+                    <button id="bookc" class="session-button" type="button">Book</button>
+                  </div>
+                </div>
+              </div>             
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer-top" id="give">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6 footer-contact">
+            <h3>AFL</h3>
+            <p>
+              Apostle Femi Lazarus is a man commissioned by God to go raise
+              Gods' End Times Armies. He is the founder of the Femi Lazarus 
+              Ministries Ecumenical, popularly called FLAME. He is also the 
+              founder and Senior Pastor of Sphere of Light Church headquartered
+              in Ibadan. He is a Public Health Professional and the Convener of 
+              Campus on Fire Conference. 
+            </p>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#about">About me</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#book">Media Libray</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#counsel">Book a Session</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#contact">Invite me</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#give">Give</a></li>
+
+            </ul>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-links">
+            <h4>Others</h4>
+            <p>
+              Kindly use the following account details to Patner with Femi
+              Lazarus Ministries International.<br>
+              <div class="row">
+                <div class="col-md-6">
+                  <strong>Naira Account:</strong> <br>
+                  <strong>Account Name: </strong> Femi Lazarus Ministries <br>
+                  <strong>Bank Name:</strong>  UBA <br>
+                  <strong>Account Number: </strong> 2215005289<br>
+                </div>
+                <div class="col-md-6">
+                  <strong>Dollar Account:</strong> <br>
+                  <strong>Account Name:</strong> Sphere of Light Church <br>
+                  <strong>Bank Name:</strong>  UBA <br>
+                  <strong>Account Number:</strong>  3003979764<br>
+                  <strong>Swift Code:</strong>  UNAFNGLA<br>
+                </div>
+              </div>              
+            </p>
+            <p>
+              Connect with Apostle Femi Lazarus on every social media platforms
+            </p>
+            <div class="social-links mt-3">
+              <a target="_blank" href="https://twitter.com/lazarusfemi2?t=1u0PtiMdhZIhxPl8V0oiqQ&s=09" class="twitter"><i class="bx bxl-twitter"></i></a>
+              <a target="_blank" href="https://instagram.com/femilazarus1?igshid=YmMyMTA2M2Y=" class="instagram"><i class="bx bxl-instagram"></i></a>
+              <a target="_blank" href="https://www.youtube.com/@femilazarus3722" class="google-plus"><i class="bx bxl-youtube"></i></a>
+              <a target="_blank" href="https://t.me/pastorfemilazarus" class="linkedin"><i class="bx bxl-telegram"></i></a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container footer-bottom clearfix">
+      <div class="copyright">
+        &copy; Copyright <strong><span>2023</span></strong>. Femi Lazarus Ministries International
+      </div>     
+    </div>
+  </footer>
+  <!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/js/main.js"></script>
+  
+  <script type="text/javascript">
+    function ValidateForm(){
+      console.log("Validating");     
+      var mname = $("#mname").val();   
+      var location = $("#location").val();  
+      var mdate = $("#mdate").val(); 
+      var number = $("#number").val(); 
+      var visitorMail = $("#email").val();  
+      var digit = /^[0-9]+$/;
+      var email = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+      var valid = true;
+      removeMessage();    
+      
+      
+      if(mname == ""){
+              event.preventDefault();
+              document.getElementById("cmname").innerHTML="This input cannot be left empty";
+              valid=false;
+      } 
+      if(location == ""){
+              event.preventDefault();
+              document.getElementById("clocation").innerHTML="This input cannot be left empty";
+              valid=false;
+      } 
+      if(mdate == ""){
+              event.preventDefault();
+              document.getElementById("cdate").innerHTML="Please select a meeting date";
+              valid=false;
+      } 
+      if(!digit.test(number)){
+              event.preventDefault();
+              document.getElementById("cnumber").innerHTML="Input a valid Phone Number";
+              valid=false;
+      }
+      if(!email.test(visitorMail)){
+              event.preventDefault();
+              document.getElementById("cemail").innerHTML="Input a valid Email Address";
+              valid=false;
+      }
+      return valid;   
+
+           
+    }
+
+  function removeMessage(){
+      var eremove = document.querySelectorAll(".error");
+      [].forEach.call(eremove, function(el){
+          el.innerHTML="";
+      });
+  }
+
+
+  $("#invite").click(function(e){
+          e.preventDefault();
+              var valid = ValidateForm();
+              if (valid===true){
+                var form = $(this).closest('form');
+                var data = form.serialize();
+                var url = "invite.php";
+                $.ajax({
+                  type:'post',
+                  url:url,
+                  data: data,
+                  success: function(response){
+                    console.log("Success");                      
+                  },
+                  error : function(request,error){
+                    console.log("failure");                      
+                  }   
+                });
+              }               
+  });
+
+
+</script>
+
+</body>
+
+</html>
